f0e6853343182524f6bd2bbd1e9ba316a10ac042
Cristian Molinari <cris.molinari96@gmail.com>
2023-03-01 14:50:48 +0100

    add list desserts

--- src/Desserts.java
+++ src/Desserts.java
@@ -1,25 +1,42 @@
 public class Desserts {
     // ATTRIBUTES
     private String name;
+    private String ingredients;
     private String description;
     private double weight;
     private double price;
 
     // CONSTRUCTOR
-    Desserts(String name, String description, double weight, double price){
+    Desserts(String name, String ingredients,String description, double weight, double price){
         this.name = name;
+        this.ingredients = ingredients;
         this.description = description;
         this.weight = weight;
         this.price = price;
     }
 
+    // METHODS
+    public void printInfo(){
+        System.out.println(" - Dessert : " + getNameDessert());
+        System.out.println("     - Ingredients : " + getIngredientsDessert());
+        System.out.println("     - Description : " + getDescriptionDessert());
+        System.out.println("     - Weight : " + getWeightDessert() + " g ");
+        System.out.println("     - Price :  " + getPriceDessert() + "€ ");
+//        System.out.println("Dessert : " + name + "Description : " + description + " weight : " + weight + " price : " + price)
+    }
+
+
+
+
     // GETTERS
     public String getNameDessert(){return name;}
+    public String getIngredientsDessert(){return ingredients;}
     public String getDescriptionDessert(){return description;}
     public double getWeightDessert(){return weight;}
     public double getPriceDessert(){return price;}
     // SETTERS
     public void setNameDessert(String name){this.name = name;}
+    public void setIngredientsDessert(String ingredients){this.ingredients = ingredients;}
     public void setDescriptionDessert(String description){this.name = description;}
     public void setWeightDessert(double weight){this.weight = weight;}
     public void setPriceDessert(double price){this.price = price ;}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 
 public class Main {
     public static void main(String[] args) {
+        // - DESSERTS
+        Desserts appleCake = new Desserts("Apple cake  " , "sugar, vanilla, eggs, butter, flour, yeast, milk, apple granny smith" , " American apple pie with", 1.000, 12.0);
+        Desserts tiramisù = new Desserts("Tiramisù  " , "mascarpone cheese, egg yolk, savoiardi biscuits, coffee, cocoa powder"," - Dessert with coffe, biscuits, cocoa", 2.000, 10.0);
+        Desserts saltyChocolate = new Desserts("Salty chocolate ice cream  " , "water, sugar, milk, chocolate fondant, chocolate milk, cocoa"," - Ice-cream with chocolate fondant and milk chocolate", 500, 25.0);
+        Desserts sacherTorte = new Desserts("Sachertorte  " , "chocolate fondant, cream, butter, sugar, eggs, flour "," - Dessert with lemon cream", 1.000, 25.0);
+        Desserts composeYourDessert = new Desserts("Compose your dessert  " ,"available ingredients : Chocolate, pistachio, cream, salty-chocolate ice-cream ",  "depending on the choice it is possible to make a cake, an ice cream or a mini dessert",500, 25.0);
+
+        System.out.print("1) ");
+        appleCake.printInfo();
+
+        System.out.println();
+
+        System.out.print("2)");
+        tiramisù.printInfo();
+        System.out.println();
+
+        System.out.print("3)");
+        saltyChocolate.printInfo();
+        System.out.println();
+
+        System.out.print("4)");
+        sacherTorte.printInfo();
+        System.out.println();
+
+        System.out.print("5)");
+        composeYourDessert.printInfo();
+        System.out.println();
 
     }
 }
