2f35fda687f103c34e1a8108cd8f6d8f1fb6c39e
pesto <giulio.pastore2410@gmail.com>
2023-04-03 18:07:46 +0200

    Added the Javadoc to all constructors and methods; added the isAlcoholic method to beverage.

--- src/Desserts.java
+++ src/Desserts.java
@@ -1,18 +1,24 @@
 import java.util.Set;
 
-public class Desserts extends Course{
-    private double weight;
-    /**
-     * @param name name of the dessert
-     * @param description description of dessert
-     * @param weight weight of dessert
-     * @param price price of dessert
+public class Desserts extends Course {
+
+    /***
+     * This is the constructor for the Desserts class
+     * @param name        Dessert's name
+     * @param description Dessert's description
+     * @param weight      Dessert's weight
+     * @param calories    Dessert's calories
+     * @param price       Dessert's price
+     * @param mt          Dessert's menu type
+     * @param allergens   Dessert's allergens
      */
+
     public Desserts(String name, String description, double weight, double calories, double price, MenuType mt, Set<AllergensEnum> allergens) {
-        super(name, description, calories, price, mt, allergens);
-        this.weight = weight;
+        super(name, description, weight, calories, price, mt, allergens);
         this.courseType = CourseEnum.DESSERTS;
     }
-    public double getWeightDessert(){return weight;}
+
+    public double getWeightDessert() {return weight;}
+
     public void setWeight(double weight) {this.weight = weight;}
 }
--- src/Starters.java
+++ src/Starters.java
@@ -1,27 +1,24 @@
 import java.util.Set;
 
 public class Starters extends Course {
-    private double weight;
 
-    /**
+    /***
+     * This is the constructor for the Starters class
      * @param name        Starter's name
-     * @param allergens   Set of allergens
      * @param description Starter's description
-     * @param calories    calories of the Starter
-     * @param price       price of the Starter
+     * @param weight      Starter's weight
+     * @param calories    Starter's calories
+     * @param price       Starter's price
+     * @param mt          Starter's menu type
+     * @param allergens   Starter's allergens
      */
 
     public Starters(String name, String description, double weight, double calories, double price, MenuType mt, Set<AllergensEnum> allergens) {
-        super(name, description, calories, price, mt, allergens);
-        this.weight = weight;
+        super(name, description, weight, calories, price, mt, allergens);
         this.courseType = CourseEnum.STARTERS;
     }
 
-    public double getWeight() {
-        return weight;
-    }
+    public double getWeight() {return weight;}
 
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
+    public void setWeight(double weight) {this.weight = weight;}
 }
