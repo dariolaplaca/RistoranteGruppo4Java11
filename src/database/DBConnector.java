f615e791a28f1cdc8121de8223c62df3d907217a
Marco Massaro <marco.massaro01@gmail.com>
2023-05-08 22:40:05 +0200

    ADD: ATTEMPT adding Table to DB

--- src/database/DBConnector.java
+++ src/database/DBConnector.java
@@ -37,6 +37,14 @@ public class DBConnector {
                 MAX_NUMBER_OF_CUSTOMERS INT
                 );
                 """);
+//ATTEMPTING TO ADD TABLE: TABLE TO THE DB. SORRY SE FA SCHIFO
+        schema.executeUpdate("""
+                CREATE TABLE IF NOT EXISTS TABLE(
+                ID INT AUTO_INCREMENT PRIMARY KEY,
+                RESTAURANT_ID INT NOT NULL,
+                FOREIGN KEY (RESTAURANT_ID) REFERENCES RESTAURANT(ID)
+                );
+                """);
 
         connection.close();
     }
