7c532d5b89278cc8386e5a960949d30027195fdf
Federico Scozzari <federicommscozzari@gmail.com>
2023-05-15 18:59:15 +0200

    add TableDAO

--- src/database/DAO/TableDAO.java
+++ src/database/DAO/TableDAO.java
@@ -7,11 +7,17 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TableDAO {
-    public void insertIntoTable() throws SQLException {
+    public void insertIntoTable(Integer tableNumber, Integer customerId, Integer orderId, Integer numberOfSeats) throws SQLException {
         Connection connection = DBConnector.getConnection();
-        String sqlInsertValues = "INSERT INTO RESTAURANT_TABLE() VALUES();";
-
+        String sqlInsertValues = "INSERT INTO RESTAURANT_TABLE(TABLE_NUMBER, CUSTOMER_ID, TABLE_ORDER_ID, NUMBER_OF_SEATS) VALUES(?, ?, ?, ?);";
         PreparedStatement preparedStatement = connection.prepareStatement(sqlInsertValues);
+        preparedStatement.setInt(1, tableNumber);
+        preparedStatement.setInt(2, customerId);
+        preparedStatement.setInt(3, orderId);
+        preparedStatement.setInt(4, numberOfSeats);
+        preparedStatement.executeUpdate();
+        connection.close();
+
     }
 
 }
